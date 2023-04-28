9ad243e98e9c0d1da353d6769b9eb3e88b6f25ef
kala-111 <113574035+kala-111@users.noreply.github.com>
2023-04-28 11:40:42 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,42 +1,108 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorila/mux"
+)
+
+type Book struct {
+	ID            string  `json:"id"`
+	Name          string  `json:"name"`
+	Isbn          string  `json:"isbn"`
+	Discount      string  `json:"discount"`
+	TypeOfTheBook string  `json:"typeofthebook"`
+	Price         *Price  `json:"price"`
+	Author        *Author `json:"author"`
+}
+
+type Author struct {
+	Id               string `json:"id"`
+	Name             string `json:"name"`
+	NoOfBooksWritten string `json:"noofbookswritten"`
+	Place            string `json:"place"`
+	Language         string `json:"language"`
+}
+
+type Price struct {
+	OnlinePrice  string `json:"onlineprice"`
+	OfflinePrice string `json:"offlineprice"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range books {
+		if item.ID == params["ID"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+
+	}
+	json.NewEncoder(w).Encode("Item not found")
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.Id == params["ID"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			json.NewDecoder(r.Body).Decode(&book)
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.ID == params["ID"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+
+}
 
 func main() {
-	months := 12
+	r := mux.NewRouter()
 
-	switch months {
-	case 1:
-		fmt.Println("january")
-	case 2:
-		fmt.Println("february")
-	case 3:
-		fmt.Println("march")
-	case 4:
-		fmt.Println("april")
-	case 5:
-		fmt.Println("may")
-	case 6:
-		fmt.Println("june")
-	case 7:
-		fmt.Println("july")
-	case 8:
-		fmt.Println("august")
-	case 9:
-		fmt.Println("september")
-	case 10:
-		fmt.Println("october")
-	case 11:
-		fmt.Println("november")
-	case 12:
-		fmt.Println("december")
+	books: = append(books, Book{ID: "1", Name: "Kavya", TypeOfTheBook: "comedy", Isbn: "4", Discount: "15", Author: &Author{Id: "SS", Name: "Rajamoule", NoOfBooksWritten: "2", Place: "Chennai", Language: "Tamil"}, Price: &Price{OnlinePrice: "700", OfflinePrice: "1000"}})
 
-	default:
-		fmt.Println("something else")
-	}
-	if months == 5 {
-		fmt.Println("months")
-	} else {
-		fmt.Println("something esle")
-	}
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/book/{ID}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{ID}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{ID}", deleteBook).Methods("DELETE")
+
+	fmt.Println("Starting server on 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
